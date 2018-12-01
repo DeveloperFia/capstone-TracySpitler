edeaaa3313a07062232b3aefb8e37b246c55bf57
Tracy Spitler <tracyspitler@gmail.com>
2018-12-01 18:42:23 -0500

    updated app.js to include pug templates in folder 'views'

--- app.js
+++ app.js
@@ -2,7 +2,11 @@
 const express = require('express');
 const app = express();
 
-// add router to app
+// pug - template engine
+app.set('views', __dirname + '/views');
+app.set('view engine', 'pug');
+
+// routes
 const index = require('./routes/index');
 app.use('/', index);
 
