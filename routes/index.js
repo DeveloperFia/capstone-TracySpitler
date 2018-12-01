bf72a4d0cf60295978ffd87a0be331e3ec77b319
Tracy Spitler <tracyspitler@gmail.com>
2018-12-01 12:42:34 -0500

    updated routes/index.js - created route for default page. sends 'hello world' to the response

--- routes/index.js
+++ routes/index.js
@@ -2,5 +2,11 @@
 const express = require('express');
 const router = express.Router()
 
+// default page
+router.get('/', (req, res, next) => {
+    // send hello world
+    res.send('<h1>Hello World</h1>');
+})
+
 // set up router
 module.exports = router;
