567fa566737fdcbd01f4d0d7ccb17562e594d66d
Tracy Spitler <tracyspitler@gmail.com>
2018-12-01 16:46:46 -0500

    updated routes/index.js - added basic 404 error handling for non-existant routes

--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,16 @@ const router = express.Router()
 router.get('/', (req, res, next) => {
     // send hello world
     res.send('<h1>Hello World</h1>');
-    // set status to 200
-    res.status(200);
 })
+
+// 404 error handling
+router.use(function(req, res, next) {
+    // if the route doesn't exist..
+    if (!req.route)
+        // let the user know
+        res.status(404).send('<h1>Uh oh! 404 error: page not found</h1><br><a href="/">Go Home</a>');
+    next();
+});
 
 // set up router
 module.exports = router;
