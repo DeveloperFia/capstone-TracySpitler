2e901a6b3ecaa4011d1d0d600dad28b861fb3397
Tracy Spitler <tracyspitler@gmail.com>
2018-12-04 14:47:39 -0500

    updated app.js - handles database connection errors and outputs to console

--- app.js
+++ app.js
@@ -9,6 +9,15 @@ const mongoose = require('mongoose');
 // connect to the database
 mongoose.connect('mongodb://'+process.env.MONGO_HOST+'/'+process.env.MONGO_DATABASE);
 
+// get default connection
+var db = mongoose.connection;
+
+// bind connection to error event (to get notification of connection errors)
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+// bind connection to connection event (to get notification of connection)
+db.once('open', () => console.log('DATABASE CONNECTED SUCCESSFULLY') );
+
 // pug - template engine
 app.set('views', __dirname + '/views');
 app.set('view engine', 'pug');
