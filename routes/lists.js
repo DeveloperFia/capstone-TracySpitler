5465158970ab2cc3ccb73db1a431c8d83332c690
Tracy Spitler <tracyspitler@gmail.com>
2018-12-06 13:47:07 -0500

    updated routes/lists.js - created a test list from schema and output to console

--- routes/lists.js
+++ routes/lists.js
@@ -1,9 +1,20 @@
 // include
 const express = require('express');
 const router = express.Router()
+const List = require('../models/List');
 
 // all lists page
 router.get('/lists', (req, res, next) => {
+    let test = new List({name: 'Test List', difficulty: 2});
+    test.validate((err) => {
+        console.log('err',err);
+    })
+    console.log('\n\rtesting!');
+    console.log('\n\rname:', test.name,
+                '\n\rdifficulty:', test.difficulty,
+                '\n\rcreated_at:', test.created_at,
+                '\n\rupdated_at:', test.updated_at,
+                '\n\rsongs:', test.songs);
     // render pug template - signup
     res.render('all-lists');
 })
