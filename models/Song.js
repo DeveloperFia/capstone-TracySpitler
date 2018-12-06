e3e2c58c2f0187857e4d2f3b6e1c42c48607e5a5
Tracy Spitler <tracyspitler@gmail.com>
2018-12-06 17:33:47 -0500

    updated models/Song.js - refined properties and requirements; created pre save function to save the date_added

--- models/Song.js
+++ models/Song.js
@@ -5,16 +5,30 @@ var songSchema = mongoose.Schema({
     // and custom error message if missing
     title: {type: String, required: [true, 'a song needs a title']},
     artist: {type: String, required: [true, 'an artist needs a name']},
+    chords: [],
+    capo: {type: Number, min: 0, max: 10},
     tempo: Number,
     duration: Number,
     key: Number,
     mode: Boolean,
     time_signature: Number,
-    spotify_uri: String,
-    spotify_id: String,
+    date_added: Date,
+    last_played: Date,
+    spotify_uri: {type: String, unique: [true, 'uri already exists'], sparse: true},
+    spotify_id: {type: String, unique: [true, 'spotify id already exists'], sparse: true},
     // relationship
     // an array of list ids that the song is in
-    songs: [{type: mongoose.Schema.Types.ObjectId, ref: 'Song'}],
+    lists: [{type: mongoose.Schema.Types.ObjectId, ref: 'List'}],
 });
+
+songSchema.pre('save', function(next) {
+
+  var currentDate = new Date();// if their is no date added set it to now
+  if(!this.date_added) {
+    this.date_added = currentDate;
+  }
+  // save the model to the database
+  next();
+})
 
 module.exports = mongoose.model('Song', songSchema)
