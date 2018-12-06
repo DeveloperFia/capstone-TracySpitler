874344f4e26026b9a5c71e5fe7db75351576705e
Tracy Spitler <tracyspitler@gmail.com>
2018-12-06 17:35:17 -0500

    updated routes/lists.js - created the Library list and a test song to make sure everything is working

--- routes/lists.js
+++ routes/lists.js
@@ -1,21 +1,42 @@
 // include
 const express = require('express');
 const router = express.Router()
+
+// models
 const List = require('../models/List');
+const Song = require('../models/Song');
 
 // all lists page
 router.get('/lists', (req, res, next) => {
-    let test = new List({name: 'Test List', difficulty: 2});
-    test.validate((err) => {
-        console.log('err',err);
+    // list test
+    let list = new List({name: 'Library', deletable: false});
+    // validate
+    list.validate((err) => {
+        if (err)
+            console.log('err', err);
     })
-    console.log('\n\rtesting!');
-    console.log('\n\rname:', test.name,
-                '\n\rdifficulty:', test.difficulty,
-                '\n\rcreated_at:', test.created_at,
-                '\n\rupdated_at:', test.updated_at,
-                '\n\rsongs:', test.songs);
-    // render pug template - signup
+    // // save to the database
+    // list.save(function(err) {
+    //     if (err)
+    //         console.log('err', err);
+    //     else console.log('List saved successfully!');
+    // });
+
+    // song test
+    let song = new Song({title: 'Dirty Paws', artist: 'Of Monsters and Men', lists: '5c098659425abb9e0a17aab4', capo: 3});
+    // validate
+    song.validate((err) => {
+        if (err)
+            console.log('err', err);
+    })
+    // // save to the database
+    // song.save(function(err) {
+    //     if (err)
+    //         console.log('err', err);
+    //     else console.log('Song saved successfully!');
+    // });
+
+    // render pug template - all-lists
     res.render('all-lists');
 })
 
