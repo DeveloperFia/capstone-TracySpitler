9d5a1ac9f797ee0552c9d07854e9dc325194a03c
Tracy Spitler <tracyspitler@gmail.com>
2018-12-08 19:27:40 -0500

    updated routes/lists.js - created a POST request to save the list to the db (validated on client and server side then saved)

--- routes/lists.js
+++ routes/lists.js
@@ -33,14 +33,57 @@ router.get('/lists', getLists, (req, res, next) => {
 })
 
 // create route to get and render person.pug and pass data
-router.get('/lists/create', getLists, (req, res, next) => {
+router.get('/expand', getLists, (req, res, next) => {
 
     // render the page with the lists passed as data
-    res.render(path.join(__dirname, '/../views/create-list.pug'), {
+    res.render(path.join(__dirname, '/../views/expand-library.pug'), {
         name: "Tracy Spitler",
         github: "https://github.com/TracySpitler",
         lists: req.lists,
     });
+})
+
+// route to save a new list - a POST request to /lists
+router.post('/lists', getLists, (req, res, next) => {
+
+    // check for empty values
+    req.checkBody('list_name', 'Please give the list a name.').notEmpty();
+
+    // if there are errors..
+    var errors = req.validationErrors();
+    if (errors){
+        // render the form with errors
+        res.render(path.join(__dirname, '/../views/expand-library.pug'), {
+            list_errors: errors,
+            name: "Tracy Spitler",
+            github: "https://github.com/TracySpitler" });
+            return;
+    }
+    // otherwise save the list to the db
+    else {
+        // create a new list object
+        var newList = List({
+            name: req.body.list_name,
+            difficulty: req.body.list_difficulty,
+        });
+
+        // save list to the database
+        newList.save(function(err) {
+            if (err) {
+                // render the form with errors
+                res.render(path.join(__dirname, '/../views/expand-library.pug'), {
+                    list_errors: err,
+                    db_error: "This list already exists! Please give it a different name.",
+                    name: "Tracy Spitler",
+                    github: "https://github.com/TracySpitler" });
+            }
+            else {
+                console.log('List saved successfully!')
+                // go back to the home page
+                res.redirect('/lists');
+            }
+        });
+    }
 })
 
 // set up router
