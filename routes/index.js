9fe9fd16d7bad6021f4f38634ba0685c6476d1c1
Tracy Spitler <tracyspitler@gmail.com>
2018-12-14 00:03:46 -0500

    updated routes/index.js - added routes for logging in, logging out, and basic user profile

--- routes/index.js
+++ routes/index.js
@@ -22,5 +22,21 @@ router.get('/start', (req, res, next) => {
     res.render('auth');
 })
 
+// POST ** log in
+router.post('/login', passportLocal.authenticate('local', {failureRedirect: '/start'}), (req, res, next) => {
+    res.redirect('/profile');
+});
+
+// log out
+router.get('/logout', (req, res, next) => {
+    req.logout();
+    res.redirect('/');
+});
+
+// user profile
+router.get('/profile', (req, res, next) => {
+    res.render('profile', {user: req.user});
+});
+
 // set up router
 module.exports = router;
