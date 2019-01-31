421bf27f1b090a18b7b9b546402c5ffa5092ace5
Tracy Spitler <tracyspitler@gmail.com>
2019-01-31 14:18:51 -0500

    updated models/User.js - User schema for the database

--- models/User.js
+++ models/User.js
@@ -1,10 +1,19 @@
 let mongoose = require('mongoose');
-
 var userSchema = mongoose.Schema({
-    // set the name of a property and the datatype, requirement and custom error message if missing
     name: String,
-    username: {type: String, required: [true, 'a user needs an email'], unique: true},
+    username: {type: String, required: true, unique: true},
     password: String
 })
+
+userSchema.statics.findOneOrCreate = function findOneOrCreate(condition, doc, callback) {
+  const self = this;
+  self.findOne(condition, (err, result) => {
+    return result
+      ? callback(err, result)
+      : self.create(doc, (err, result) => {
+        return callback(err, result);
+      });
+  });
+};
 
 module.exports = mongoose.model('User', userSchema);
