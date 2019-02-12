f58ec4259def800ca8e4f3bb3f0fd7cf5137ef62
Tracy Spitler <tracyspitler@gmail.com>
2019-02-12 12:03:31 -0500

    updated Mongo URI

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 // EXAMPLE STRING: mongodb://username:password@hostname:port/database
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
+mongoose.connect('mongodb://'+process.env.MONGO_HOST+'/'+process.env.MONGO_DATABASE, { useNewUrlParser: true });
 
 // Get the default connection
 var db = mongoose.connection;
